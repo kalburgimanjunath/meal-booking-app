f58c2c456f435357bfa651a1f24aa3c17d9b1693
Nsubuga Solomon <snsubuga@live.com>
2018-04-22 20:18:47 +0300

    Fix TypeError for datatable and datepicker not being functions

--- ui/js/admin.js
+++ ui/js/admin.js
@@ -10,33 +10,3 @@ function showMenu() {
 function deleteItem() {
   alert("Are you sure you want to delete this meal option.");
 }
-
-$(document).ready(function() {
-  $("#dtable").DataTable({
-    responsive: true,
-    order: [[0, "desc"]]
-  });
-
-  $("#menuDate").datepicker({
-    dateFormat: "dd/mm/yy"
-  });
-
-  //trigger displaying and closing of a modal
-  var modal = document.querySelector(".modal");
-  var trigger = document.querySelector(".trigger");
-  var closeButton = document.querySelector(".close-button");
-
-  function toggleModal() {
-    modal.classList.toggle("show-modal");
-  }
-
-  function windowOnClick(event) {
-    if (event.target === modal) {
-      toggleModal();
-    }
-  }
-
-  trigger.addEventListener("click", toggleModal);
-  closeButton.addEventListener("click", toggleModal);
-  window.addEventListener("click", windowOnClick);
-});
