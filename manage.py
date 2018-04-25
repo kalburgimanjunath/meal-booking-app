86c78a191eb18ba1e94472eb9e9447436b58db44
Nsubuga Solomon <snsubuga@live.com>
2018-04-25 21:26:10 +0300

    chore: renamed mealapp.py to manage.py

---
+++ manage.py
@@ -0,0 +1,29 @@
+import os
+from app import create_application
+from flask_script import Manager, Shell
+from app.models import User, data
+
+app = create_application(os.getenv('MEAL_APP_CONFIG') or 'default')
+
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, User=User, data=data)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+@manager.command
+def test():
+    "Function to run unit tests"
+    import unittest
+    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    return not result.wasSuccessful()
+
+
+if __name__ == '__main__':
+    manager.run()
