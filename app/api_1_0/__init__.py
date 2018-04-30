29d603918586a8f74234a69c5f3f3ed9e55e011d
Nsubuga Solomon <snsubuga@live.com>
2018-04-30 15:38:05 +0300

    chore: Add mock data

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -1,6 +1,25 @@
 from flask import Blueprint
 from flask_restplus import Api
+from ..models import User, MealOption
 
+# Add some mock data for api testing
+# create a test user
+test_user = User(name='test', email='test@test.com')
+test_user.password = 'test'
+test_user.save()
+
+# create a test admin user
+admin_user = User('admin', email='admin@admin.com')
+admin_user.password = 'admin'
+admin_user.is_admin = True
+admin_user.save()
+
+# create a test meal
+meal = MealOption('lorem meal', 10000)
+meal.save()
+
+meal = MealOption('Beef with rice', 1500)
+meal.save()
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
