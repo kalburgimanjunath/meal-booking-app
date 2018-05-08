b5584e259d3f1a26b1e7b18090dcf863ed7b2d0c
Nsubuga Solomon <snsubuga@live.com>
2018-05-08 20:31:46 +0300

    feat: Add database configs

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     DEBUG = True
     SECRET_KEY = os.getenv('SECRET_KEY') or 'Andela-is-awesome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     ORDER_EXPIRES_IN = 5
 
     @staticmethod
@@ -11,12 +12,27 @@ class Config:
         pass
 
 
+class DevConfig(Config):
+    """
+      Configurations for development
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DEV_DATABASE_URL',
+        'postgresql://andela1:AnDELa@localhost/bookamealdb')
+
+
 class TestingConfig(Config):
+    """
+     Configurations for testing
+    """
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DEV_DATABASE_URL',
+        'postgresql://andela1:AnDELa@localhost/test_bookamealdb')
 
 
 config = {
     'testing': TestingConfig,
 
-    'default': Config
+    'default': DevConfig
 }
