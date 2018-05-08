b2e235f399563097bf0267c87360779f5568acfd
Nsubuga Solomon <snsubuga@live.com>
2018-05-08 20:32:59 +0300

    feat: Add sqlalchemy

--- manage.py
+++ manage.py
@@ -1,18 +1,21 @@
 import os
-from app import create_application
+from app import create_application, db
 from flask_script import Manager, Shell
-from app.models import User, data, MealOption
+from app.models import User, Meal
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_application(os.getenv('MEAL_APP_CONFIG') or 'default')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, User=User, data=data)
+    return dict(app=app, User=User)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
 
 
 @manager.command
@@ -23,6 +26,18 @@ def test():
     result = unittest.TextTestRunner(verbosity=2).run(tests)
 
     return not result.wasSuccessful()
+
+
+@manager.command
+def initialize():
+    """
+     Initialize application by inserting roles
+    """
+    from flask_migrate import upgrade
+    from app.models import Role
+
+    upgrade()
+    Role.insert_roles()
 
 
 if __name__ == '__main__':
