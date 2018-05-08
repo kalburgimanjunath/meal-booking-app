5d2f5e655afca8548d1312ab6c573e5c9bd50416
Nsubuga Solomon <snsubuga@live.com>
2018-05-08 20:36:15 +0300

    feat: Add models for persisting data

--- app/models.py
+++ app/models.py
@@ -5,32 +5,63 @@ This module contains models of entities showing how they relate to each other
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
-import datetime
-from flask import current_app
+from . import db
 
 
-class data:
+class Permission:
     """
-     This class holds the mock data objects used to store application data
+     Permission represents persmissions held by a role
     """
-    users = []
-    menus = []
-    meals = []
-    caterings = []
-    orders = []
+    CUSTOMER = 2
+    CATERER = 4
 
 
-class User:
+class Role(db.Model):
     """
-    User class for storing data about a user of the application
+     Role represents the roles table
     """
 
-    def __init__(self, name, email):
-        self.id = None
-        self.name = name
-        self.email = email
-        self.password_hash = None
-        self.is_admin = False
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Customer': (Permission.CUSTOMER, True),
+            'Admin': (Permission.CATERER, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+
+class User(db.Model):
+    """
+    User class represents the users table
+    """
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    orders = db.relationship('Order', backref='customer', lazy='dynamic')
+    created_at = db.Column(db.DateTime(), default=db.func.current_timestamp())
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            self.role = Role.query.filter_by(default=True).first()
 
     @property
     def password(self):
@@ -43,74 +74,53 @@ class User:
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def save(self):
-        if not data.users:
-            self.id = 1
-        else:
-            u = data.users[-1]
-            self.id = u.id + 1
-        data.users.append(self)
-
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
-            'email': self.email,
-            'is_admin': self.is_admin
+            'email': self.email
         }
 
-    @staticmethod
-    def get_by_email(val):
-        for u in data.users:
-            if u.email == val:
-                return u
-        return None
+    def can(self, permissions):
+        return self.role is not None and \
+            (self.role.permissions & permissions) == permissions
 
-    @staticmethod
-    def get_by_id(val):
-        for u in data.users:
-            if u.id == val:
-                return u
-        return None
+    def is_administrator(self):
+        return self.can(Permission.CATERER)
 
     def generate_jwt_token(self):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_in=36000)
-        return s.dumps({
-            'id': self.id,
-            'name': self.name,
-            'email': self.email
-        }).decode('ascii')
+        s = Serializer(current_app.config['SECRET_KEY'],
+                       expires_in=360000)
+        return s.dumps({'id': self.id}).decode('ascii')
 
     @staticmethod
     def verify_jwt_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-
+        """
+         Verifies a JWT token
+        """
+        serializer = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user = s.loads(token)
+            data = serializer.loads(token)
         except:
             return None
-        return User.get_by_id(user['id'])
+        return User.query.get(data['id'])
 
 
-class Catering:
+class Catering(db.Model):
     """
-    Catering class for storing data about catering business managed by admin
+    Catering class represents Catering class
     """
-
-    def __init__(self, name, address):
-        self.id = None
-        self.name = name
-        self.address = address
-        self.admin = None
-        self.meal_options = []
-        self.menus = []
-
-    def save(self):
-        if not data.caterings:
-            self.id = 1
-        else:
-            self.id = data.caterings[-1].id + 1
-        data.caterings.append(self)
+    __tablename__ = 'caterings'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100))
+    menus = db.relationship('Menu', backref='catering', lazy='dynamic')
+    meals = db.relationship('Meal', backref='catering', lazy='dynamic')
+    orders = db.relationship('Order', backref='catering', lazy='dynamic')
+    admin_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    admin = db.relationship(
+        "User", backref=db.backref('catering', uselist=False))
+    created_at = db.Column(db.DateTime(), default=db.func.current_timestamp())
 
     def to_dict(self):
         return {
@@ -120,69 +130,58 @@ class Catering:
         }
 
 
-class Menu:
+menu_meals = db.Table('menu_meals',
+                      db.Column('menu_id', db.Integer, db.ForeignKey(
+                          'menus.id'), primary_key=True),
+                      db.Column('meal_id', db.Integer, db.ForeignKey(
+                          'meals.id'), primary_key=True)
+                      )
+
+
+class Menu(db.Model):
     """
-    Menu class fot storing data about a menu
+    Menu class represents the menus table
     """
 
-    def __init__(self, title, description):
-        self.id = None
-        self.title = title
-        self.description = description
-        self.meals = []
-        self.menu_date = None
-        self.catering = None
-
-    def save(self):
-        if not data.menus:
-            self.id = 1
-        else:
-            self.id = data.menus[-1].id + 1
-        data.menus.append(self)
-
-    def add_meals(self, meals):
-        for meal_id in meals:
-            meal = MealOption.get_by_id(int(meal_id))
-        if meal:
-            self.meals.append(meal)
+    __tablename__ = 'menus'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text)
+    date = db.Column(db.Date)
+    meals = db.relationship('Meal', secondary=menu_meals, lazy='subquery',
+                            backref=db.backref('menu', lazy=True))
+    catering_id = db.Column(db.Integer, db.ForeignKey('caterings.id'))
+    created_at = db.Column(db.DateTime(), default=db.func.current_timestamp())
 
     def to_dict(self):
+        """
+          Turns Menu into a dict for easy serialization
+        """
         return {
             'id': self.id,
             'title': self.title,
             'description': self.description,
             'meals': [meal.to_dict() for meal in self.meals],
-            'menu_date': str(self.menu_date)
+            'menuDate': str(self.date)
         }
 
 
-class MealOption:
+class Meal(db.Model):
     """
-    MealOption for storing data about a meal
+    Meal class represents the meals table
     """
-
-    def __init__(self, title, price, description=None):
-        self.id = None
-        self.title = title
-        self.price = price
-        self.description = description
-        self.catering = None
-
-    def save(self):
-        if not data.meals:
-            self.id = 1
-        else:
-            self.id = data.meals[-1].id + 1
-        data.meals.append(self)
-
-    @staticmethod
-    def get_by_id(id):
-        for meal in data.meals:
-            if meal.id == id:
-                return meal
-        return None
+    __tablename__ = 'meals'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text)
+    catering_id = db.Column(db.Integer, db.ForeignKey('caterings.id'))
+    created_at = db.Column(db.DateTime(), default=db.func.current_timestamp())
 
     def to_dict(self):
+        """
+          Turns Meal into a dict for easy serialization
+        """
         return {
             'id': self.id,
             'title': self.title,
@@ -191,38 +190,36 @@ class MealOption:
         }
 
 
-class Order:
+order_meals = db.Table('order_meals',
+                       db.Column('order_id', db.Integer, db.ForeignKey(
+                           'orders.id'), primary_key=True),
+                       db.Column('meal_id', db.Integer, db.ForeignKey(
+                           'meals.id'), primary_key=True)
+                       )
+
+
+class Order(db.Model):
     """
-    Order class for storing data about customer order
+    Order class represents the orders table
     """
-
-    def __init__(self):
-        self.id = None
-        self.meals = []
-        self.total_cost = 0
-        self.user = User(None, None)
-        self.catering = None
-        self.expires_at = datetime.datetime.now(
-        ) + datetime.timedelta(minutes=current_app.config['ORDER_EXPIRES_IN'])
-
-    def save(self):
-        if not data.orders:
-            self.id = 1
-        else:
-            self.id = data.orders[-1].id + 1
-        data.orders.append(self)
-
-    @staticmethod
-    def get_by_id(id):
-        for order in data.orders:
-            if order.id == id:
-                return order
-        return None
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    total_cost = db.Column(db.Float, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    catering_id = db.Column(db.Integer, db.ForeignKey('caterings.id'))
+    meals = db.relationship('Meal', secondary=order_meals, lazy='subquery',
+                            backref=db.backref('order', lazy=True))
+    expires_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     def to_dict(self):
+        """
+         Turns order into dict for easy serialization
+        """
         return {
             'id': self.id,
             'cost': self.total_cost,
             'expiresAt': str(self.expires_at),
-            'meals': [meal.to_dict() for meal in self.meals]
+            'meals': [meal.to_dict() for meal in self.meals],
+            'customer': self.customer.to_dict()
         }
